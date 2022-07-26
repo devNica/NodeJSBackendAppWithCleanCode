319a5576f730fd6ec33358f37c874bfc709b0116
devNica <designsdevnicaragua@gmail.com>
2022-07-26 11:50:55 -0600

    :sparkles: User Login

--- src/infrastructure/express/api/routes/auth.router.ts
+++ src/infrastructure/express/api/routes/auth.router.ts
@@ -3,10 +3,11 @@ import { authControllerFactory } from '@factories/auth/auth-controller-factory'
 import { expressRouterAdapter } from '@infrastructure/express/adapters/express-route-adapter'
 import { Router } from 'express'
 
-const { userRegisterController } = authControllerFactory()
+const { userRegisterController, userLoginController } = authControllerFactory()
 
 const authRouter = Router()
 
 authRouter.post('/signup', expressRouterAdapter(userRegisterController))
+authRouter.post('/signin', expressRouterAdapter(userLoginController))
 
 export default authRouter
