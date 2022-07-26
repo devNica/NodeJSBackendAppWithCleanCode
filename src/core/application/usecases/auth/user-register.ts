f820be46ad124bc0dc928f4510b805b4bb714979
devNica <designsdevnicaragua@gmail.com>
2022-07-26 10:16:44 -0600

    :lock: plain to hash password

--- src/core/application/usecases/auth/user-register.ts
+++ src/core/application/usecases/auth/user-register.ts
@@ -1,18 +1,21 @@
 import constants from '@common/constants/constants'
 import { getCurrentTimeFormatted } from '@common/helpers/date-utility'
+import { CredentialSecurity } from '@common/security/credential-security'
 import { FindUserByEmailRepository } from '@core/application/ports/repositories/find-user-repository'
 import { UserRegisterRepository } from '@core/application/ports/repositories/user-register-repository'
 import { UserRegisterRequest, UserRegisterResponse, UserRegisterUC } from '@core/domain/models/user'
 
 export class UserRegisterUseCase implements UserRegisterUC {
   constructor (
+    private readonly passwordhash: CredentialSecurity,
     private readonly userRegisterRepository: UserRegisterRepository,
     private readonly findUserByEmailRepository: FindUserByEmailRepository
   ) {}
 
   async execute (request: UserRegisterRequest): Promise<UserRegisterResponse> | never {
     const dto = {
-      ...request,
+      email: request.email,
+      password: await this.passwordhash.hash(request.password),
       createdAt: getCurrentTimeFormatted(),
       fkGroup: constants.ROL.COMMON_USERS
     }
@@ -25,5 +28,5 @@ export class UserRegisterUseCase implements UserRegisterUC {
 
     if (newUser === null) throw new Error('Oops, an error has ocurred')
     return { id: newUser.id }
-  };
+  }
 }
--- src/factories/auth/auth-controller-factory.ts
+++ src/factories/auth/auth-controller-factory.ts
@@ -1,3 +1,4 @@
+import { CredentialSecurity } from '@common/security/credential-security'
 import { UserRegisterUseCase } from '@core/application/usecases/auth/user-register'
 import { UserRegisterResponse } from '@core/domain/models/user'
 import { findUserByEmailRepository, userRegisterRepository } from '@infrastructure/repositories'
@@ -7,6 +8,7 @@ import { ControllerGenericResponse } from '@interface/responses/controller-gener
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const authControllerFactory = () => {
   const userRegisterUC = new UserRegisterUseCase(
+    new CredentialSecurity(),
     userRegisterRepository,
     findUserByEmailRepository
   )
